23139b2f9d1bf3dc9bcc256c5746fff942a36699
alamin <alamin@gmail.com>
2021-09-22 13:40:19 +0600

    Button

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const App = () => {
+  const onPressLearnMore = () => {
+    console.log('Hello world');
+  };
   return (
     <View style={[styles.container, styles.horizontal]}>
-      <Text>Hello world</Text>
-      <ActivityIndicator />
-      <ActivityIndicator size="large" />
-      <ActivityIndicator size="large" color="#0000ff" />
+      <Button
+        onPress={onPressLearnMore}
+        title="Click me"
+        color="#841584"
+        accessibilityLabel="learn more about this purple button"
+      />
     </View>
   );
 };
@@ -16,11 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
+    padding: 20,
   },
 });
 
