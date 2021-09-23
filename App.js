1ad96e0c2defad116153f9467b2c648bfbd92087
alamin <alamin@gmail.com>
2021-09-23 08:18:02 +0600

    Text

--- App.js
+++ App.js
@@ -1,37 +1,24 @@
-import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
+  const [titleText, setTitleText] = useState("Birds's Nest");
+  const bodyText = 'This is sample text';
+
+  const onPresstitle = () => {
+    setTitleText("I'm from setTitleText [hello]");
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        resizeMode="cover"
-        source={{
-          uri: 'https://media-exp1.licdn.com/dms/image/C5603AQFe6INPvSY3tA/profile-displayphoto-shrink_200_200/0/1632061449207?e=1637798400&v=beta&t=7yoQrfdoGb2Y7cIuYXVA4VbmTXZuxitMKn-J1l8CiD0',
-        }}>
-        <Text style={styles.text}>Inside</Text>
-      </ImageBackground>
+    <View>
+      <Text>
+        <Text>{titleText}</Text>
+      </Text>
+      <Text onPress={onPresstitle}>{bodyText}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
